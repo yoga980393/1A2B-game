acdf8093f677aa65ea39c8846088e9092638f0c9
yoga980393 <wggisdde@gmail.com>
2023-02-14 15:39:14 +0800

    test2

--- Test/Program.cs
+++ Test/Program.cs
@@ -26,6 +26,8 @@ namespace Test
                     numbers[r] = temp;
                 }
 
+                //12345
+
                 //Console.WriteLine($"{numbers[0]}{numbers[1]}{numbers[2]}{numbers[3]}");
 
                 while (true)
